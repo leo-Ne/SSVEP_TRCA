ecae4b1960576a1d1c7e13c107cee7dd7324c794
leo-Ne <xieshiyao_tju@163.com>
2020-11-17 22:24:51 +0800

    function trca() completed!

--- TRCA.py
+++ TRCA.py
@@ -25,52 +25,66 @@ class TRCA:
         # some other para 
         self.W      = np.zeros([smpls], np.float32)
         self.models = None
-        print('Initialization Done!')
         pass
 
-    def loadData(self, eegFile:str, Freq_Phase:str, block=0):
+    def loadData(self, eegFile:str, labelFile:str, block=0):
         """
         Create a dictory variable by loading *.mat file which include eeg data and labels
         """
         eegData = loadmat(eegFile)
         eegData = np.array(eegData['data'], np.float32)
 
-        freq_phase = loadmat(Freq_Phase)
+        freq_phase = loadmat(labelFile)
         freq_phase = np.array([freq_phase['freqs'], freq_phase['phases']], np.float32)
         freq_phase = freq_phase[:, 0, :].T
         print(r'freq_phase shape:', '\t', np.shape(freq_phase))
         print(r'SSVEP EEG shape:', '\t', np.shape(eegData))
+        print('labels:')
+        print('\tfreqs: \tphases: \t')
+        for i, item in enumerate(freq_phase):
+            print('label'+str(i)+'\t', item[0], '\t', item[1])
         self.eegData, self.freq_phase = eegData, freq_phase
         self.shape                    = eegData.shape
+        self.W                        = np.zeros([self.shape[2]], np.float32)
         return eegData, freq_phase
 
-    def trca(self):
+    def trca(self, label:int):
         """
         Firstly, calculate the CovMax S.
         Secondly, calculate the coefficients W by Rayleigh-Ritz method.
+        For each label, W was stored as a vector(chans, 1).
         """
-        eegData = self.eegData.copy()
-        chans, smpls, trials = self.shape
-        print("shape:\t", chans, smpls, trials)
-        S = np.zeros([chans])
+        # labels
+        freq_phase = self.freq_phase.copy()
+        eegData    = self.eegData.copy()
+        # SSVEP EEG for one freqs
+        trainData  = eegData[:, :, label, :]
+        chans, smpls, trials = np.shape(trainData)
+        S = np.zeros([chans, chans], np.float32)
+        # calculate the matrix S
         for trial_i in range(trials):
-            x1 = eegData[:, :, trial_i]
-            x1 = x1 - np.mean(x1, axis=1)
+            xi = trainData[:, :, trial_i]
+            xi = xi - np.mean(xi, axis=1)[:, None]
             for trial_j in range(trial_i+1, trials):
-                x2 = eegData[:, :, trial_j]
-                x2 = x2 - np.mean(x2, axis=1)
-                S  += np.matmul(x1, x2.T) + np.matmul(x2, x1.T)
+                xj  = trainData[:, :, trial_j]
+                xj  = x2 - np.mean(xj, axis = 1)[:, None]
+                S  += np.matmul(xi, xj.T) + np.matmul(xi, xj.T)
         ## end for
         ## Those code is working in a ineffective way! change it!
-        UX = np.reshape(eegData,[chans, smpls*trials])
-        UX = UX - np.mean(UX, axis=1)
+
+        UX = np.reshape(trainData,[chans, smpls*trials])
+        UX = UX - np.mean(UX, axis=1)[:, None]
         Q = np.matmul(UX, UX.T)
-        QS = np.matmul(S, Q)
-        # Q-1 * S maybe lead the wrong result!
-        eigenvalues, eigenvectaors = np.linalg.eig(QS)
+        Q_inv = np.linalg.inv(Q)
+        QS = np.matmul(Q_inv, S)
+        # cal w 
+        eigenvalues, eigenvectors = np.linalg.eig(QS)
         maxEigenvealue_i = np.argwhere(eigenvalues==np.max(eigenvalues))
-        w = eigenvectaors[maxEigenvealue_i]
-        self.W += w
+        w = eigenvectors[maxEigenvealue_i][0, 0]
+        w = w[:, None]
+        print("W : \t", w.T)
+        print("W shape :\t", np.shape(w))
+        self.W = w
         return w
        
     def model(self):
@@ -95,6 +109,8 @@ def testUnit():
     labelFileName = dirName + labelFile
     session       = TRCA()
     session.loadData(dataFileName, labelFileName, block=0)
+    session.trca(label=0)
+    print('testUnit Passed!')
     pass
 
 
