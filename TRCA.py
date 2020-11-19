1057373aa8d8b073c198ff3f6b4a6a1fa3f4a2e1
leo-Ne <xieshiyao_tju@163.com>
2020-11-19 23:17:50 +0800

    test TRCA model generalization

--- TRCA.py
+++ TRCA.py
@@ -12,33 +12,64 @@ import matplotlib.pyplot as plt
 import mne
 
 class TRCA:
-    def __init__(self):
+    def __init__(self, _objFile:str, _labelFile:str):
         """
         Initialize the class TRCA and load eeg data;
         Load train data only with one block;
         """
+        # file names
+        self.objFile   = _objFile
+        self.labelFile = _labelFile
         # Initilization with None and 0
-        self.eegData, self.freq_phase = None, None
-        chans, smpls, trials, block   = 0, 0, 0, 0
-        self.shape                    = np.array([chans, smpls, trials, block], np.int32)
+        self.eegData    = None
+        self.trainData  = None
+        self.testData   = None
+        self.freq_phase = None
+        chans, smpls, trials, block = 0, 0, 0, 0
+        self.shape                  = np.array([chans, smpls, trials, block], np.int32)
         # para about Models
-        self.W = np.zeros([trials, chans])
-        self.models = np.zeros([trials, smpls])
+        self.W      = None
+        self.models = None
         pass
 
-    def loadData(self, eegFile:str, labelFile:str, testBlock=-1):
+    def loadData(self, testBlock=-1):
         """
         Create a dictory variable by loading *.mat file which include eeg data and labels
         """
-        eegData = loadmat(eegFile)
-        eegData = np.array(eegData['data'], np.float32)
-        freq_phase = loadmat(labelFile)
-        freq_phase = np.array([freq_phase['freqs'], freq_phase['phases']], np.float32)
-        freq_phase = freq_phase[:, 0, :].T
-        self.eegData, self.freq_phase = eegData, freq_phase
-        self.shape                    = eegData.shape
-        self.W                        = np.zeros([self.shape[2]], np.float32)
-        return eegData, freq_phase
+        print("Loading  files:\t")
+        print(self.objFile)
+        print(self.labelFile)
+        print('testBlock:\t', testBlock)
+        eegData    = self.eegData    
+        freq_phase = self.freq_phase
+        if self.eegData is None:
+            eegData         = loadmat(self.objFile)
+            eegData         = np.array(eegData['data'], np.float32)
+        if self.freq_phase is None:
+            freq_phase      = loadmat(self.labelFile)
+            freq_phase      = np.array([freq_phase['freqs'], freq_phase['phases']], np.float32)
+            freq_phase      = freq_phase[:, 0, :].T
+        ######################slice data##############################
+        trainData = None
+        testData  = None
+        _, _, _, blocks = eegData.shape
+        if testBlock in range(0, blocks):
+            testData = eegData[:, :, :, testBlock]
+            if testBlock != 0 or testBlock != blocks:
+                trainData = np.concatenate((eegData[:,:,:,:testBlock], eegData[:,:,:,testBlock+1:]), axis=3)
+            elif testBlock == 0:
+                trainData = eegData[:,:,:,1:]
+            elif testBlock == blocks-1:
+                trainData = eegData[:,:,:,blocks-1]
+        else:
+            trainData = eegData
+        ######################slice data End##########################
+        self.eegData    = eegData
+        self.trainData  = trainData
+        self.testData   = testData
+        self.freq_phase = freq_phase
+        self.shape      = eegData.shape
+        return trainData, freq_phase
 
     def trca(self, label:int):
         """
@@ -48,9 +79,9 @@ class TRCA:
         """
         # labels
         freq_phase = self.freq_phase.copy()
-        eegData    = self.eegData.copy()
+        trainData = self.trainData.copy()
         # SSVEP EEG for one freqs
-        trainData  = eegData[:, :, label, :]
+        trainData  = trainData[:, :, label, :]
         chans, smpls, trials = np.shape(trainData)
         S = np.zeros([chans, chans], np.float32)
         # calculate the matrix S
@@ -63,7 +94,6 @@ class TRCA:
                 S  += np.matmul(xi, xj.T) + np.matmul(xi, xj.T)
         ## end for
         ## Those code is working in a ineffective way! change it!
-
         UX    = np.reshape(trainData,[chans, smpls*trials])
         UX    = UX - np.mean(UX, axis = 1)[:, None]
         Q     = np.matmul(UX, UX.T)
@@ -76,36 +106,37 @@ class TRCA:
         w      = w[:, None]
         return w
        
-    def train(self, objFile:str, labelFile:str):
+    def train(self):
         """
         By using the training data and W, function create models for classification.
         This step to decrease the computational requirements when TRCA works in hard device.
         """
-        print("----------------- In Function train() -----------------")
+        print("------------- In Function train() -------------")
         ######################load data#################################
-        if self.eegData is None:
-            self.loadData(eegFile=objFile, labelFile=labelFile)
+        if self.trainData is None:
+            self.loadData()
             print("Loading eeg Data...")
-            eegData  = self.eegData.copy()
+            trainData  = self.trainData.copy()
         else:
-            eegData = self.eegData.copy()
+            print("Data already loaded!")
+            trainData = self.trainData.copy()
         ######################cal WX####################################
-        chans, smpls, _, _ = self.shape
+        chans, smpls, _, _ = self.trainData.shape
         labels_length      = np.shape(self.freq_phase)[0]
         print("chans:\t", chans, "smpls:\t", smpls)
         print('label_length:\t', labels_length)
         W = np.zeros([labels_length, chans], np.complex64)
-        for label in range(labels_length-38):
+        for label in range(labels_length):
             w           = self.trca(label = label)
             W[label, :] = np.squeeze(w)[:]
             print("\rIn %d/40 template calculating" % (label+1), flush=True, end='')
         print('\nW shape:\t', W.shape)
         # models
         # cal average value of each signal in each trial and chanel of all blocks
-        mean_val  = np.mean(eegData, axis=1)[:, None, :, :]
-        eegData   = eegData - mean_val
-        sumSignal = np.sum(eegData, axis=3)
-        print('eegData shape:\t', eegData.shape)
+        mean_val  = np.mean(trainData, axis=1)[:, None, :, :]
+        trainData   = trainData - mean_val
+        sumSignal = np.sum(trainData, axis=3)
+        print('trainData shape:\t', trainData.shape)
         print('sumSignal shape:\t', sumSignal.shape)
         WX = np.zeros([labels_length, smpls], np.complex64)
         for i in range(labels_length):
@@ -113,27 +144,40 @@ class TRCA:
             x     = sumSignal[:, :, i]
             wx    = np.matmul(w, x)
             WX[i] = wx[:]
+        ######################cal WX END################################
         self.models = WX
-
+        print("------------- End Function train() ------------")
         return WX
 
-    def classification(self, testData:str, model=None):
-        if model is None:
-            self.train()
-        pass
+    def classification(self):
+        """
+        Classification() is to test generalization of TRCA model by using 5-1 floder.  
+        """
+        if self.models is None:
+            print("There is not models! Please train it!")
+            return -1
+        _, _, _, blocks = self.shape
+        for testBlk in range(blocks):
+            self.loadData(testBlock=testBlk)
 
+        return 1
 
 def testUnit():
+    print("----------------- In Function testUnit() -----------------")
+    print("-----------------   Test Unit start      -----------------")
     dirName       = r'./tsing/'
     eegFile       = r'S6.mat'
     labelFile     = r'Freq_Phase.mat'
     dataFileName  = dirName + eegFile
     labelFileName = dirName + labelFile
-    session       = TRCA()
-    session.loadData(dataFileName, labelFileName)
+    session       = TRCA(_objFile=dataFileName, _labelFile=labelFileName)
+    session.loadData(testBlock=1)
+    session.loadData(testBlock=2)
 #    session.trca(label=0)
-    session.train(objFile=dataFileName, labelFile=labelFileName)
-    print('\n\ntestUnit Passed!')
+    session.train()
+    session.classification()
+    print('testUnit Passed!')
+    print("-----------------   Test Unit End        -----------------")
     pass
 
 
