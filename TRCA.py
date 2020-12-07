e073dca954aed713c4501ab02e26820c0385f1b8
leo-Ne <xieshiyao_tju@163.com>
2020-12-07 18:28:15 +0800

    update

--- TRCA.py
+++ TRCA.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 import mne
 
 class TRCA():
-    def __init__(self, _Subject=None, begin=0.14, tuse=1.0, fs=250):
+    def __init__(self, _Subject=None, fs=250):
         self._eegData   = None
         self._dataShape = np.zeros([5],np.int32)
         # [nChennel, nSample, nEvent, nTrial, nBlock]
@@ -24,24 +24,45 @@ class TRCA():
         self._result    = None
         self.label      = None      # Equal to event
         # TRCA Setting
-        self._begin     = begin
-        self._tuse      = tuse
-        self.fs         = fs
+        self._begin     = 0.14
+        self._tuse      = 1.0
+        self._fs        = fs
         pass
 
     def loadData(self, filename):
         # process
+        data, dataUse = None, None 
         if self._eegData is None:
             data = loadmat(filename)['data']
             chnls = np.array([47, 53, 54, 55, 56, 57, 60, 61, 62], np.int32)
             dataUse = data[chnls, :, :, :].copy()
+            self._eegData = dataUse.copy()
             # filter
-            del data
+            print('Data was firstly loaded!')
         else:
-            dataUse = self._eegData.copy()
-        self._eegData = dataUse.copy()
-        del dataUse
+            print('Data was already loaded!')
+        del data, dataUse
         return
+
+    def cutData(self, tBegin=-1, tuse=-1):
+        fs = self._fs
+        data = self._eegData.copy()
+        if tBegin == -1 or tuse == -1 :
+            tBegin = self._begin
+            tEnd   = tBegin + self._tuse
+            print('Data cut by default!')
+        else:
+            tEnd   = tBegin + tuse
+            self._begin = tBegin
+            self._tuse  = tuse
+            print('Data cut by setting!')
+        nBegin = np.int32(fs * tBegin)
+        nEnd   = np.int32(fs * tEnd)
+#        tUseW  = np.array([nBegin, nEnd], np.int32)
+        dataUse = data[:, nBegin:nEnd, :, :]
+        self._eegData = dataUse.copy()
+        del data, nBegin, nEnd, dataUse
+        return  
     
     def SSVEPFilter(self, b=None, a=None):
         data = self._eegData.copy()
@@ -92,12 +113,13 @@ class TRCA():
 def unitTest():
     # Unit test
     sub6 = r'./tsing/S6.mat'
-    session = TRCA(_Subject=6, begin=0.14, tuse=1,fs=250)
+    session = TRCA(_Subject=6,fs=250)
+    print("eegData shape:\t", np.shape(session._eegData))
     session.loadData(filename=sub6)
-    session.loadData(filename=sub6)
-    session.loadData(filename=sub6)
-    session.loadData(filename=sub6)
-    session.loadData(filename=sub6)
+    print("eegData shape:\t", np.shape(session._eegData))
+    print("tBegin tEnd fs:\t", session._begin, session._tuse, session._fs)
+    session.cutData()
+    print("eegData shape:\t", np.shape(session._eegData))
     pass
 
 if __name__ == "__main__":
